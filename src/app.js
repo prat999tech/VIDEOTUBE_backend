5420c821113b06218706416349ff6177a5f4485f
prat999tech <prateeksingh26@outlook.com>
2025-05-21 18:38:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,6 +27,28 @@ import userRouter from "./routes/user.routes.js"
 app.use("/api/v1/users",userRouter) //jaise hi kisi ne "/api/v1/users" likha toh control chla zyega "userRouters" pr wha se hum "user.routes.js" file pr chle jayenge
 //yaha pr humara url agr humne users likha h "http://localhost:8000/api/v1/users"" toh iss url ke through control user controller file m chla jayega fir wha pr humne alg alg route likhe honge jaise hume agr register pr jaana ha ya contacts pr jaana
 //"/api/v1/users"-yeh sirf likhna ka tarika ha
+
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 export {app};
 
 
